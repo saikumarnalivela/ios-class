d507b525074208eaae0f9bdd515f1c874f242662
sai <sainalivela@gmail.com>
2019-02-14 10:59:07 -0600

    full commit

---
+++ HelloAutoLayout/HelloAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloAutoLayout
+//
+//  Created by Nalivela,Saikumar on 2/14/19.
+//  Copyright © 2019 Nalivela,Saikumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
